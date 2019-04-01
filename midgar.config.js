43876c31a58ea9a618beca3edfc221cd8c537cdc
Gregory PIVA <gpiva@edl.fr>
2019-04-01 12:08:06 +0200

    bdd add insert function

--- midgar.config.js
+++ midgar.config.js
@@ -2,20 +2,24 @@
 
 const path = require('path');
 const mainPath = path.parse(process.mainModule.filename).dir;
+let config = {};
+
+try {
+  config = require(mainPath + '/midgar.config.js');
+} catch(e) {}
 
 module.exports = () => {
-  try {
-    return require(mainPath + '/midgar.config.js');
-  } catch(e) {
-    return configDefault;
-  };
-}
-
-const configDefault = {
-  log: {
-    level: 3,
-    type: "console",
-    dir: "./logs/",
-    name: "log-server"
+  return {
+    mode: config.mode || 'development',
+    log: {
+      level: config.log.level || 0,
+      type: config.log && config.log.type || "console",
+      dir: config.log && config.log.dir || "./logs/",
+      name: config.log && config.log.name || "log-server"
+    },
+    bdd: {
+      development: config.bdd && config.bdd.development || null,
+      production: config.bdd && config.bdd.production || null
+    }
   }
-};
+}
